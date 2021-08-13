dcd1fa4f713a66d6f73c7905e3f88a5b8e17bd22
Anderson <andersoncleyson@protonmail.com>
2021-08-13 09:11:44 -0300

    grupo de rotas no express

--- app.js
+++ app.js
@@ -2,18 +2,20 @@
 //Carregango módulos
 const express = require('express')
 
-const handlebars = require('handlebars')
+const handlebars = require('express-handlebars')
 
 const bodyParser = require('body-parser')
 
 const app = express()
 
+const admin = require("./routes/admin")
+
 //const mongoose = require('mongoose')
 
 // Configurações
 // Body Parser
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 //Handlebars
 app.engine('handlebars', handlebars({defaultLayout: 'main'}))
@@ -24,6 +26,8 @@ app.set('view engine', 'handlebars')
     // Em breve
 
 // Rotas
+
+app.use('/admin', admin)
 
 // Outros
 
