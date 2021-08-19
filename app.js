bec4d38940fbeff2978408ca94d3a0ce3909d1b2
Anderson <andersoncleyson@protonmail.com>
2021-08-19 08:11:14 -0300

    listagem de postagem

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ const session = require('express-session')
 
 const flash = require("connect-flash")
 
+require("./models/Postagem")
+
+const Postagem = mongoose.model("postagens")
+
 // Configurações
 app.use(session({
     secret: "anything",
@@ -54,6 +58,39 @@ app.use(express.static(path.join(__dirname,"public")))
 
 
 // Rotas
+
+app.get("/", (req, res) => {
+    Postagem.find().lean().populate("categories").sort({data: "desc"}).then((postagens) => {
+        res.render("index", {postagens: postagens})
+        
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro interno")
+        res.redirect("/404")
+    })
+    
+})
+
+app.get("/postagem/:slug", (req, res) => {
+    Postagem.findOne({slug: req.params.slug}).lean().then((postagem) => {
+        if(postagem){
+            res.render("postagem/index", {postagem: postagem})
+        }else{
+            req.flash("error_msg", "Esta postagem não existe")
+            res.redirect("/")
+        }
+    }).catch((err) => {
+        req.flash("error_msg", "houve um erro" + err)
+        res.redirect("/")
+    })
+})
+
+app.get("/404", (req, res) => {
+    res.send('Erro 404')
+})
+
+app.get("/posts", (req, res) => {
+    res.send("Lista posts")
+})
 
 app.use('/admin', admin)
 
