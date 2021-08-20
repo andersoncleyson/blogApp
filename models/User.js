670a901b64a5a54be9ed29a604c63f0db370e65f
Anderson <andersoncleyson@protonmail.com>
2021-08-20 09:07:29 -0300

    definindo model de user

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const User = new Schema({
+    nome:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+mongoose.nodel("user", User)
